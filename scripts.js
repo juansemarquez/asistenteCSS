b47f892cfc2b251f0a9af52b14cf64df8f139603
juansemarquez <juansemarquez@yahoo.com.ar>
2013-08-03 18:12:54 -0300

    Se puede pegar código para las 3 primeras pestañas (falta esquinas)

--- scripts.js
+++ scripts.js
@@ -361,7 +361,7 @@ function salvarRadius() {
     var regla;
     for (var i=0; i<codigo.length; i++) {
         regla=codigo[i];
-        if(regla.indexOf('radius')==-1) {
+        if(regla.indexOf('radius')!=-1) {
 			radius = radius + regla;
 		}
     }
@@ -850,45 +850,145 @@ function setearInputs(regla) {
     //######################################
     //                 +BORDE
     //######################################
+    else if (prop.indexOf("border")!=-1&&prop.indexOf("radius")==-1)
+    {
+        return bordePegarCodigo(prop,valor)
+    }
+}
+
+function bordePegarCodigo(prop,valor) {    
+    var posicion; 
+    var pos;   
+    if (prop.indexOf("top-")!=-1) {
+        posicion="ARRIBA";
+        pos="top-"
+        habilitarBordes(false);
+        document.getElementById("cuatroBordes").checked=false;
+    }
+    else if(prop.indexOf("bottom-")!=-1) {
+        posicion="ABAJO";
+        pos="bottom-";
+        habilitarBordes(false);
+        document.getElementById("cuatroBordes").checked=false;
+    }
+    else if(prop.indexOf("left-")!=-1) {
+        posicion="IZQUIERDA";
+        pos="left-";
+        habilitarBordes(false);
+        document.getElementById("cuatroBordes").checked=false;
+    }
+    else if(prop.indexOf("right-")!=-1) {
+        posicion="DERECHA";
+        pos="right-";
+        habilitarBordes(false);
+        document.getElementById("cuatroBordes").checked=false;    
+    }
+    else {
+        posicion="";
+        pos="";
+        habilitarBordes(true);
+        document.getElementById("cuatroBordes").checked=true;
+    }    
+    //alert(prop+' XXX '+valor+' XXX '+posicion+' XXX '+pos);
+    if (prop=="border-"+pos+"style") {
+        if(valor=="none"||valor=="hidden"||valor=="solid"||valor=="dotted"||valor=="outset"||
+        valor=="dashed"||valor=="double"||valor=="groove"||valor=="ridge"||valor=="inset") {
+            document.getElementById("tipo_borde"+posicion).value=valor;
+            return true;        
+        }
+        else { return false; }
+    }
+    else if (prop=="border-"+pos+"width") {
+        return constaDeNumeroYUnidad(prop,valor,"tamanoBorde",posicion);        
+    }
+    else if (prop=="border-"+pos+"color") {
+        if (valor.indexOf("rgba(")==-1) {
+            if(valor[0]!='#') {
+                valor = convertirNombreColorHexa(valor);
+                //alert(valor);
+                if (valor=="Error") {return false;}       
+            }
+            if(valor.length==7||valor.length==4) {           
+                //Si el color está expresado en tres caracteres, lo represento con 6 y quito #:            
+                if(valor.length==4) {
+                    var col = valor[1]+valor[1]+valor[2]+valor[2]+valor[3]+valor[3];
+                }
+                //Si no, le quito el # solamente
+                else {
+                    var col=valor.substr(1);
+                }
+                //Valido que solo se use 0-9 A-F
+                var validos = "0123456789ABCDEF";
+                col=col.toUpperCase();
+                for(var i=1; i<6; i++) {
+                    if(validos.indexOf(col[i])==-1) {
+                        return false;
+                    }
+                }   
+                document.getElementById("color_borde"+posicion).value=col;
+                var unEntero = parseInt(col, 16);
+                var r = (unEntero >> 16) & 255;
+                var g = (unEntero >> 8) & 255;
+                var b = unEntero & 255;
+                r = Math.round((r/255)*10000)/100;
+                g = Math.round((g/255)*10000)/100;
+                b = Math.round((b/255)*10000)/100;
+                document.getElementById("bordeRGB"+posicion).value=r + "%," + g + "%," + b + "%";
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {            
+            //Obtengo el valor de la transparencia:
+            var transp = valor.substr(valor.lastIndexOf(",")+1);
+            //Le saco el parentesis final:
+            transp = transp.substr(0,transp.length-1);
+            //Asigno en controles            
+            document.getElementById("transparencia_borde"+posicion).value=transp;
+            return true;
+        }
+    }
 }
 
 
-function constaDeNumeroYUnidad(prop,valor) {
-        var aux = separarUnidadNumero(valor);
-        if(aux[0]=="Error") {return false;}        
-        var unidad = aux[1];
-        var val = aux[0];
-        document.getElementById(prop).value=val;
-        document.getElementById(prop + "_unidades").value=unidad;  
-        return true;
+function constaDeNumeroYUnidad(prop,valor,id="",posicion="") {
+    if(id==""||id.length==0) {id=prop;}
+    var aux = separarUnidadNumero(valor);
+    if(aux[0]=="Error") {return false;}        
+    var unidad = aux[1];
+    var val = aux[0];
+    document.getElementById(id+posicion).value=val;
+    document.getElementById(id + "_unidades"+posicion).value=unidad;  
+    return true;
 }
     
 function separarUnidadNumero(valor) {
-        if(valor=="0") {
-            var nroUnidad=new Array("0","px");
-            return nroUnidad;
-        }
-        var unidad;
-        var numero;                
-        if (valor[valor.length-1]=="%") {
-            numero=valor.substr(0,valor.length-1);
-            unidad="%";            
-        }
-        else {
-            unidad=valor[valor.length-2]+valor[valor.length-1];
-            numero=valor.substr(0,valor.length-2);            
-        }
-        //alert(numero+' NU '+unidad);
-        if (unidad!="%"&&unidad!="px"&&unidad!="em"&&unidad!="cm"&&unidad!="mm"&&unidad!="in"&&unidad!="pt"&&unidad!="ex") {
-            //La unidad no es reconocida
-            var nroUnidad=new Array("Error","Error");
-        }
-        else {
-            
-            var nroUnidad=new Array(numero,unidad);
-        }
-        //alert (nroUnidad[0] + 'NU' + nroUnidad[1]);
+    if(valor=="0") {
+        var nroUnidad=new Array("0","px");
         return nroUnidad;
+    }
+    var unidad;
+    var numero;                
+    if (valor[valor.length-1]=="%") {
+        numero=valor.substr(0,valor.length-1);
+        unidad="%";            
+    }
+    else {
+        unidad=valor[valor.length-2]+valor[valor.length-1];
+        numero=valor.substr(0,valor.length-2);            
+    }
+    //alert(numero+' NU '+unidad);
+    if (unidad!="%"&&unidad!="px"&&unidad!="em"&&unidad!="cm"&&unidad!="mm"&&unidad!="in"&&unidad!="pt"&&unidad!="ex") {
+        //La unidad no es reconocida
+        var nroUnidad=new Array("Error","Error");
+    }
+    else {
+        var nroUnidad=new Array(numero,unidad);
+    }
+    //alert (nroUnidad[0] + 'NU' + nroUnidad[1]);
+    return nroUnidad;
 }
 function trim(cadena) {
 	return cadena.replace(/^\s+|\s+$/g,"");
@@ -898,8 +998,7 @@ function esEspacioEnBlanco(caracter) {
 	return (blancos.indexOf(caracter) != -1);
 }
 
-function convertirNombreColorHexa(nombreColor)
-{
+function convertirNombreColorHexa(nombreColor) {
     var colores = {"aliceblue":"#f0f8ff", "antiquewhite":"#faebd7", "aqua":"#00ffff", "aquamarine":"#7fffd4", "azure":"#f0ffff", "beige":"#f5f5dc", "bisque":"#ffe4c4", "black":"#000000", "blanchedalmond":"#ffebcd", "blue":"#0000ff", "blueviolet":"#8a2be2", "brown":"#a52a2a", "burlywood":"#deb887",    "cadetblue":"#5f9ea0", "chartreuse":"#7fff00", "chocolate":"#d2691e", "coral":"#ff7f50", "cornflowerblue":"#6495ed", "cornsilk":"#fff8dc", "crimson":"#dc143c", "cyan":"#00ffff",    "darkblue":"#00008b", "darkcyan":"#008b8b", "darkgoldenrod":"#b8860b", "darkgray":"#a9a9a9", "darkgreen":"#006400", "darkkhaki":"#bdb76b", "darkmagenta":"#8b008b", "darkolivegreen":"#556b2f",    "darkorange":"#ff8c00", "darkorchid":"#9932cc", "darkred":"#8b0000", "darksalmon":"#e9967a", "darkseagreen":"#8fbc8f", "darkslateblue":"#483d8b", "darkslategray":"#2f4f4f", "darkturquoise":"#00ced1",    "darkviolet":"#9400d3", "deeppink":"#ff1493", "deepskyblue":"#00bfff", "dimgray":"#696969", "dodgerblue":"#1e90ff", "firebrick":"#b22222", "floralwhite":"#fffaf0", "forestgreen":"#228b22", "fuchsia":"#ff00ff", "gainsboro":"#dcdcdc", "ghostwhite":"#f8f8ff", "gold":"#ffd700", "goldenrod":"#daa520", "gray":"#808080", "green":"#008000", "greenyellow":"#adff2f", "honeydew":"#f0fff0", "hotpink":"#ff69b4", "indianred ":"#cd5c5c", "indigo ":"#4b0082", "ivory":"#fffff0", "khaki":"#f0e68c", "lavender":"#e6e6fa", "lavenderblush":"#fff0f5", "lawngreen":"#7cfc00", "lemonchiffon":"#fffacd", "lightblue":"#add8e6", "lightcoral":"#f08080", "lightcyan":"#e0ffff", "lightgoldenrodyellow":"#fafad2", "lightgrey":"#d3d3d3", "lightgreen":"#90ee90", "lightpink":"#ffb6c1", "lightsalmon":"#ffa07a", "lightseagreen":"#20b2aa", "lightskyblue":"#87cefa", "lightslategray":"#778899", "lightsteelblue":"#b0c4de", "lightyellow":"#ffffe0", "lime":"#00ff00", "limegreen":"#32cd32", "linen":"#faf0e6", "magenta":"#ff00ff", "maroon":"#800000", "mediumaquamarine":"#66cdaa", "mediumblue":"#0000cd", "mediumorchid":"#ba55d3", "mediumpurple":"#9370d8", "mediumseagreen":"#3cb371", "mediumslateblue":"#7b68ee", "mediumspringgreen":"#00fa9a", "mediumturquoise":"#48d1cc", "mediumvioletred":"#c71585", "midnightblue":"#191970", "mintcream":"#f5fffa", "mistyose":"#ffe4e1", "moccasin":"#ffe4b5", "navajowhite":"#ffdead", "navy":"#000080", "oldlace":"#fdf5e6", "olive":"#808000", "olivedrab":"#6b8e23", "orange":"#ffa500", "orangered":"#ff4500", "orchid":"#da70d6", "palegoldenrod":"#eee8aa", "palegreen":"#98fb98", "paleturquoise":"#afeeee", "palevioletred":"#d87093", "papayawhip":"#ffefd5", "peachpuff":"#ffdab9", "peru":"#cd853f", "pink":"#ffc0cb", "plum":"#dda0dd", "powderblue":"#b0e0e6", "purple":"#800080", "red":"#ff0000", "rosybrown":"#bc8f8f", "royalblue":"#4169e1", "saddlebrown":"#8b4513", "salmon":"#fa8072", "sandybrown":"#f4a460", "seagreen":"#2e8b57", "seashell":"#fff5ee", "sienna":"#a0522d", "silver":"#c0c0c0", "skyblue":"#87ceeb", "slateblue":"#6a5acd", "slategray":"#708090", "snow":"#fffafa", "springgreen":"#00ff7f", "steelblue":"#4682b4", "tan":"#d2b48c", "teal":"#008080", "thistle":"#d8bfd8", "tomato":"#ff6347", "turquoise":"#40e0d0", "violet":"#ee82ee", "wheat":"#f5deb3", "white":"#ffffff", "whitesmoke":"#f5f5f5", "yellow":"#ffff00", "yellowgreen":"#9acd32"};
     //var x = colores[nombreColor.toLowerCase()];
     //alert(x);    
